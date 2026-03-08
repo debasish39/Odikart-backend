697f7c1745c89bd3b4ede37b1cdf56deeb80b196
Debasish Panda <156610494+debasish39@users.noreply.github.com>
2026-03-08 09:44:17 +0530

    fix cors

--- server.js
+++ server.js
@@ -11,8 +11,17 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://eshop.debasish.xyz"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 /* =============================
    MONGODB CONNECTION
 ============================= */
